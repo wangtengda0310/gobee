ff2d54896c00da4154cd02f5e12dcda8c01f7de9
王腾达 <wangtengda0310@126.com>
2024-07-12 00:52:27 +0800

    wow登录数据dump

--- realm/proxy/wow_login_proxy.go
+++ realm/proxy/wow_login_proxy.go
@@ -1,16 +1,24 @@
 // 监听tcp7324端口，将接收到的客户端数据以16进制打印在控制台后将源数据转发到logon.5awow.com，将logon.5awow.com返回的数据以16进制打印在控制台后将返回的源数据返回客户端
 
-package proxy
+package main
 
 import (
-	"encoding/hex"
+	"encoding/base64"
 	"fmt"
 	"io"
 	"log"
 	"net"
+	"os"
+	"sync"
+	"time"
 )
 
 const loginServer = "logon.5awow.com:3724"
+
+var (
+	messageID int
+	mu        sync.Mutex
+)
 
 func main() {
 	listener, err := net.Listen("tcp", ":3724")
@@ -28,7 +36,6 @@ func main() {
 		go handleConnection(clientConn)
 	}
 }
-
 func handleConnection(clientConn net.Conn) {
 	defer func(clientConn net.Conn) {
 		err := clientConn.Close()
@@ -49,12 +56,14 @@ func handleConnection(clientConn net.Conn) {
 		}
 	}(serverConn)
 	for {
-		go copyAndPrint(serverConn, clientConn)
-		copyAndPrint(clientConn, serverConn)
+		go copyAndPrint(serverConn, clientConn, "src")
+		copyAndPrint(clientConn, serverConn, "dst")
 	}
 }
 
-func copyAndPrint(dst net.Conn, src net.Conn) {
+var timestamp = time.Now().Format("20060102150405")
+
+func copyAndPrint(dst net.Conn, src net.Conn, prefix string) {
 	log.Println("\n\n\nCopying from", src.RemoteAddr(), "to", dst.RemoteAddr())
 	buf := make([]byte, 1024)
 	for {
@@ -69,7 +78,23 @@ func copyAndPrint(dst net.Conn, src net.Conn) {
 		}
 
 		data := buf[:n]
-		fmt.Println(hex.Dump(data))
+		base64Data := base64.StdEncoding.EncodeToString(data)
+
+		// Write to file
+		filename := fmt.Sprintf("%s_%s.txt", prefix, timestamp)
+		file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer file.Close()
+
+		mu.Lock()
+		messageID++
+		_, err = file.WriteString(fmt.Sprintf("%d\n%s\n", messageID, base64Data))
+		mu.Unlock()
+		if err != nil {
+			log.Fatal(err)
+		}
 
 		_, err = dst.Write(data)
 		if err != nil {
